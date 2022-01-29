07e45624bf6dc1078cf4910fa9690098509c983b
Nicolai Weitkemper <kontakt@nicolaiweitkemper.de>
2022-01-29 18:06:41 +0100

    gh-pages: fix file list

--- build_index.py
+++ build_index.py
@@ -6,6 +6,7 @@ cwd = pathlib.Path.cwd()
 
 md_files = sorted(file.relative_to(cwd) for file in cwd.rglob('*.md'))
 md_files = [str(f).replace(".tex.md", "") for f in md_files]
+md_files = [f for f in md_files if not f.startswith('gh-pages-bundle/')]
 
 
 out = ''
